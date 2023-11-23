38c330268f87331b2de92cf78cbc1695bc3fd0bb
Christophe Gakwaya <c.gakwaya@alustudent.com>
2023-11-23 01:18:13 -0800

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,5 +2,5 @@
 """  an empty class Square that defines a square """
 #creating a class
 class square:
+    """empty class that doesn't retun anything """
     pass
-""" an empty class called square """

0b1d30e0f0f1bffc0f22e03330798d7d9a54df38
Christophe Gakwaya <c.gakwaya@alustudent.com>
2023-11-24 10:29:07 -0800

    6-base_geometry.p

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Base class for geometry objects"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
